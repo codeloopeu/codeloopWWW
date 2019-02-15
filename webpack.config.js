cb1fe11f43859af2006e140dfc6c582e477aa4c7
Krzysztof Zwierzyński <krzysztof.zwierzynski@codeloop.eu>
2019-02-15 15:57:00 +0100

    added keywords and sitemap + webpack updated for sitemap.xml

--- webpack.config.js
+++ webpack.config.js
@@ -50,6 +50,10 @@ module.exports = {
       {
         from: path.resolve(__dirname, 'src/.htaccess'),
         to: path.resolve(__dirname, 'dist/')
+      },
+      {
+        from: path.resolve(__dirname, 'src/sitemap.xml'),
+        to: path.resolve(__dirname, 'dist/')
       }
     ]),
     new StyleLintPlugin({
